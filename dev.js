def3ae429e8ec52bb7edd52ad39712595afa3f27
Cory Brown <oh.wise.man@gmail.com>
2016-12-07 11:50:47 -0700

    more tests and updated demo page

--- dev.js
+++ dev.js
@@ -12,7 +12,6 @@ const Slider = ({ children }) => {
 
   return (
     <div className="slider">
-      <button onClick={prev}>{'<'}</button>
       <Track
         visibleSlides={3}
         className="track"
@@ -26,7 +25,8 @@ const Slider = ({ children }) => {
           return children
         }
       }</Track>
-      <button onClick={next}>{'>'}</button>
+      <button className="prevButton" onClick={prev}>Let me see that beard again!</button>
+      <button className="nextButton" onClick={next}>Let's see more beards!</button>
     </div>
   )
 }
@@ -36,42 +36,91 @@ Slider.propTypes = {
 }
 
 const slides = [{
-  src: 'https://placezombie.com',
+  src: 'https://placebeard.it',
   height: 300,
-  width: 300
+  width: 300,
+  joiner: 'x'
 }, {
-  src: 'https://placezombie.com',
+  src: 'https://placebeard.it',
   height: 300,
-  width: 300
+  width: 300,
+  joiner: 'x'
 }, {
-  src: 'https://placezombie.com',
+  src: 'https://placebeard.it',
   height: 300,
-  width: 300
+  width: 300,
+  joiner: 'x'
 }, {
-  src: 'https://placezombie.com',
+  src: 'https://placebeard.it',
   height: 300,
-  width: 300
+  width: 300,
+  joiner: 'x'
 }, {
-  src: 'https://placezombie.com',
+  src: 'https://placebeard.it',
   height: 300,
-  width: 300
+  width: 300,
+  joiner: 'x'
 }, {
-  src: 'https://placezombie.com',
+  src: 'https://placebeard.it',
   height: 300,
-  width: 300
+  width: 300,
+  joiner: 'x'
 }, {
-  src: 'https://placezombie.com',
+  src: 'https://placebeard.it',
   height: 300,
-  width: 300
+  width: 300,
+  joiner: 'x'
+}, {
+  text: <h1>
+    <span className="line align-right">It</span>
+    <span className="line align-right">need</span>
+    <span className="line align-right">not</span>
+    <span className="line align-right">only</span>
+    <span className="line align-right">be</span>
+    <span className="line align-right">beards!</span>
+  </h1>
+}, {
+  src: 'http://fillmurray.com',
+  height: 300,
+  width: 300,
+  joiner: '/'
+}, {
+  text: <h1>
+    <span className="line align-left">It</span>
+    <span className="line align-left">can</span>
+    <span className="line align-left">be</span>
+    <span className="line align-left">anything</span>
+    <span className="line align-left">you</span>
+    <span className="line align-left">want!</span>
+  </h1>
+}, {
+  text: <h2>Featuring:</h2>
+}, {
+  text: <p>A native scrolling "track"</p>
+}, {
+  text: <p>snap-to-slide option</p>
+}, {
+  text: <p>set the number of slide visible at a time</p>
+}, {
+  text: <p>start at any slide you want</p>
+}, {
+  text: <p>slide indecies are normalized to stay within the slide count range</p>
 }]
 
 render((
-  <Slider>
-    {slides.map(({ src, height, width }, i) => (
-      <figure className="mySlide" key={`${src}-${i}`}>
-        <img alt="Place Zombie" src={`${src}/${width}x${height}?${i}`} />
-        <figcaption>Slide Index {i}</figcaption>
-      </figure>
-    ))}
-  </Slider>
+  <div>
+    <header className="header">
+      <h1>react-track</h1>
+      <p>A carousel-like component for react</p>
+    </header>
+    <Slider>
+      {slides.map(({ src, height, width, joiner, text }, i) => (
+        <figure className="mySlide" key={`${src}-${i}`}>
+          {src && <img alt="Place Zombie" src={`${src}/${width}${joiner}${height}?${i}`} />}
+          {src && <figcaption>Slide Index {i}</figcaption>}
+          {text && <div className="text">{text}</div>}
+        </figure>
+      ))}
+    </Slider>
+  </div>
 ), document.querySelector('main'))
