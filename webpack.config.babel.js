40dd054c1e23bc54641a58743935658e85c7c7ce
zacanger <zac@zacanger.com>
2018-05-09 09:17:17 -0600

    Chore: deps

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,35 +4,41 @@ import { resolve } from 'path'
 const pub = resolve(__dirname, 'docs')
 
 export default {
+  mode: 'production',
   entry: {
     demo: './dev.js',
-    index: './src/whirligig.js'
+    index: './src/whirligig.js',
   },
   output: {
     path: pub,
     filename: '[name].js',
     library: 'react-track',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
   },
   plugins: [
     new webpack.LoaderOptionsPlugin({
       options: {
-        context: __dirname
-      }
-    })
+        context: __dirname,
+      },
+    }),
   ],
   devtool: 'inline-source-map',
   devServer: {
     host: '0.0.0.0',
     contentBase: pub,
     compress: true,
-    port: 9090
-  }
+    port: 9090,
+  },
+  performance: {
+    hints: false,
+  },
 }
