6d76f89dfe6cf9f1a1c26630023f18f8fd5a1399
zacanger <zac@zacanger.com>
2020-03-26 13:13:01 -0600

    chore: deps

--- test/slide-class.test.js
+++ test/slide-class.test.js
@@ -5,9 +5,7 @@ import Track from '../src/whirligig'
 test('slideClass prop', () => {
   const slideclass = (cn) => shallow(<Track slideClass={cn}>{() => [1]}</Track>)
 
-  expect(
-    slideclass('slideClassName')
-      .find('Slide')
-      .prop('className')
-  ).toBe('slideClassName')
+  expect(slideclass('slideClassName').find('Slide').prop('className')).toBe(
+    'slideClassName'
+  )
 })
